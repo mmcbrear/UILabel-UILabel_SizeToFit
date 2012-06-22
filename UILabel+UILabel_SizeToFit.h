f73ec7c70bd22c206678d3d371aac772907d895b
Martin McBrearty <martin@myellodigital.co.uk>
2012-06-22 13:35:42 +0100

    clean up

--- UILabel+UILabel_SizeToFit.h
+++ UILabel+UILabel_SizeToFit.h
@@ -1,10 +1,3 @@
-//
-//  UILabel+UILabel_SizeToFit.h
-//  VolvoEventToolkit
-//
-//  Created by Martin McBrearty on 22/06/2012.
-//  Copyright (c) 2012 Myello Digital Ltd. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
